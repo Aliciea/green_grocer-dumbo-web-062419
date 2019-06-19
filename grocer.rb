98950dff383c7dc5d33c0e97bacb05d65986df76
Aliciea D Suechin <adr330@nyu.edu>
2019-06-19 21:58:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,16 @@
 def consolidate_cart(cart)
   # code here
-  
+  newHash = {}
+  cart.each do |food, info|
+      food.each do |a,b|
+        if newHash[a]== nil
+          newHash[a]={price: b[:price], clearance: b[:clearance], count: 1}
+        else
+          newHash[a][:count]+=1
+        end
+      end
+  end
+  return newHash
 end
 
 def apply_coupons(cart, coupons)
